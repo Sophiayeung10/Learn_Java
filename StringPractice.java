53f32f2ed4b4068530a00f9f9e253b5ff851d5ce
Sophia Yeung <yhwhiuwai@gmail.com>
2024-09-17 15:52:34 -0400

    Add files via upload

---
+++ StringPractice.java
@@ -0,0 +1,19 @@
+public class StringPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("computer");
+        System.out.println("science");
+        int a = 6; //SET a TO 6
+        System.out.println("computer" + " " + "science" + a);
+        //DISPLAY cakesize
+
+        int temp = 72;
+        int day = 17;
+        System.out.println("Today is "+ day +" and it is "+ temp +" degrees.") 
+        //"The temperature is" + temp
+        //The student final grade is: " + grade"
+
+    }
+
+}
